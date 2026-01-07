23406285b5328178e70f53ab3ffd6f5a9cf21aba
gourav1405 <gouravsai63@email.com>
2026-01-07 18:08:18 +0530

    feat: implement basic two-player chess using WebSockets

---
+++ backend1/dist/index.js
@@ -0,0 +1,18 @@
+// import { WebSocketServer } from "ws";
+// const wss = new WebSocketServer({ port: 8080 });
+// wss.on("connection", function connection(ws) {
+//   ws.on("error", console.error);
+//   ws.on("message", function message(data) {
+//     console.log("received: %s", data);
+//   });
+//   ws.send("something");
+// });
+import { WebSocketServer } from "ws";
+import { GameManager } from "./GameManager.js";
+const wss = new WebSocketServer({ port: 8080 });
+const gameManager = new GameManager();
+wss.on("connection", function connection(ws) {
+    gameManager.addUser(ws);
+    ws.on("close", () => gameManager.removerUser(ws));
+});
+//# sourceMappingURL=index.js.map
